4f83ae0f2caaf334c65949dd06d9b950a9646b3c
krikaz <pascal.bou@protonmail.com>
2020-03-12 00:20:23 +0100

    part 1.1

---
+++ Recipes/Controller/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by krikaz on 3/12/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchField: UITextField!
+    @IBAction func searchAction(_ sender: UITextField) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
